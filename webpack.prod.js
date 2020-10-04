598158c16e7f3c79e9f4c9c686b9098fdf1183f6
pissshivers <pissshivers@protonmail.com>
2020-10-04 16:28:21 -0500

    idk: lets try custom terser config again, wtf not

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,5 +4,22 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        passes: 2,
+                        keep_classnames: true
+                    },
+                    mangle: {
+                        keep_classnames: true,
+                        reserved: [
+                            'name'
+                        ]
+                    }
+                }
+        })],
+    },
 })
